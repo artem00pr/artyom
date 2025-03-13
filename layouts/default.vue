e487564766f069937687973218010342ea6bebae
artem00pr <artyomprimak@arizona.edu>
2025-03-13 23:21:47 +0600

    gitff

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,59 +1,46 @@
 <template>
-  <header class="flex flex-row w-full bg-green-400 border-b-2 border-gray-400 max-sm:justify-between max-sm:relative">
-    <div class="basis-1/4 items-center justify-start px-8 min-w-40 max-sm:basis-auto">
-      <img src="assets/images/arizona.png" class="w-16 h-16 bg-transparent">
+  <header class="flex flex-row w-full items-center relative bg-green-400 border-b-2 border-gray-400 max-sm:justify-between max-sm:relative">
+    <div class="basis-1/4 max-sm:basis-auto justify-start px-8 min-w-40 max-sm:auto">
+      <img src="assets/images/arizonalogo.png" class="w-16 h-16 bg-transparent">
     </div>
-
-    <button 
-      class="p-2 text-white bg-gray-600 rounded-lg max-sm:block hidden" 
-      @click="toggleMenu">
-      Меню
-    </button>
-
-    <nav :class="['basis-1/2 flex flex-row items-center justify-end px-8 gap-4', { 'hidden': !showMenu }, 'max-sm:flex-col max-sm:items-center max-sm:w-full max-sm:absolute max-sm:top-full max-sm:bg-white max-sm:px-0']">
-      <NuxtLink to="/" class="my-auto p-2 hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:border-b-2 max-sm:text-center">
-        Home
-      </NuxtLink>
-      <div class="relative my-auto p-2 hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center" @click="toggleSubmenu">
-        Labs
-        <div v-show="showSubmenu" class="flex flex-col absolute top-full bg-white text-black w-56 text-center shadow-md">
-          <NuxtLink to="/lab3" class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white">Lab3</NuxtLink>
-          <NuxtLink to="/lab4" class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white">Lab4</NuxtLink>
-          <NuxtLink to="/lab5" class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white">Lab5</NuxtLink>
-          <NuxtLink to="/lab6" class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white">Lab6</NuxtLink>
-        </div>
+    <div class="basis-3/4 max-sm:basis-auto">
+      <div :class="`basis-1/2 flex flex-row justify-end gap-x-4 !burger || burger`" class="max-sm:basis-auto max-sm:top-full max-sm:w-full max-sm:flex-col max-sm:gap-0 max-sm:hidden !burger">
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/Lab1" class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">Lab1</NuxtLink>
+        <NuxtLink to="/Lab2" class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">Lab2</NuxtLink>
+        <NuxtLink to="/Lab3" class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">Lab3</NuxtLink>
+        <NuxtLink to="/Lab4" class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">Lab4</NuxtLink>
+        <NuxtLink to="/Lab5" class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">Lab5</NuxtLink>
+        <NuxtLink to="/Lab6" class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">Lab6</NuxtLink>
+        <NuxtLink to="/login" class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">Login</NuxtLink>
+        <NuxtLink to="/logout" class="my-auto p-2 border-b-2 border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">Logout</NuxtLink>
       </div>
-      <NuxtLink to="/login" class="my-auto p-2 hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">
-        LogIn
-      </NuxtLink>
-      <NuxtLink to="/logout" class="my-auto p-2 hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">
-        LogOut
-      </NuxtLink>
-    </nav>
+      <div @click="switch_burger" class="max-sm:flex max-sm:flex-col max-sm:gap-1 max-sm:justify-between max-sm:items-center max-sm:w-8 max-sm:h-6">
+        <span class="h-[3px] w-full bg-blue-950"></span>
+        <span class="h-[3px] w-full bg-blue-950"></span>
+        <span class="h-[3px] w-full bg-blue-950"></span>
+      </div>
+      <div v-else class="max-sm:flex-col max-sm:w-8 max-sm:h-6 justify-between max-sm:items-center max-sm:w-8 max-sm:h-6" @click="switch_burger">
+        <span class="h-[3px] w-full bg-blue-950 rotate-45 relative top-[9px]"></span>
+        <span class="h-[3px] w-full bg-blue-950 opacity-0"></span>
+        <span class="h-[3px] w-full bg-blue-950 relative bottom-2 rotate-[-45deg]"></span>
+      </div>
+    </div>
   </header>
 
   <main class="p-5 flex bg-green-100 h-screen">
-    <slot />
+    <slot></slot>
   </main>
 
   <footer class="flex flex-row w-full bg-green-400 border-t-2 border-gray-400 items-center justify-center px-4 py-2 gap-4">
-    <a href="https://www.youtube.com/"><img src="assets/images/youtube.png" class="w-16 h-16"></a>
-    <a href="https://github.com/"><img src="assets/images/github.png" class="w-16 h-16"></a>
-    <a href="https://www.facebook.com/"><img src="assets/images/facebook.png" class="w-16 h-16 bg-transparent"></a>
+    <a href="https://www.youtube.com/" target="_blank">
+      <img src="assets/images/youtube.png" class="w-16 h-16">
+    </a>
+    <a href="https://github.com/" target="_blank">
+      <img src="assets/images/github.png" class="w-16 h-16">
+    </a>
+    <a href="https://www.facebook.com/" target="_blank">
+      <img src="assets/images/facebook.png" class="w-16 h-16 bg-transparent">
+    </a>
   </footer>
 </template>
-
-<script setup>
-import { ref } from 'vue';
-
-const showSubmenu = ref(false);
-const showMenu = ref(false);
-
-const toggleSubmenu = () => {
-  showSubmenu.value = !showSubmenu.value;
-};
-
-const toggleMenu = () => {
-  showMenu.value = !showMenu.value;
-};
-</script>
